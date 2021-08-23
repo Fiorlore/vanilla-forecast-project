cb2a476529e74b3d8688ae0edc119e6337616ab7
Fiorlore <wkwiatki@gmail.com>
2021-08-23 11:29:51 +0200

    integrated forecast API
    added formatDay function
    API response data added to displayForecast function

--- index.js
+++ index.js
@@ -110,31 +110,42 @@ function showCelsius(event) {
   fahrenheitButton.classList.remove("active");
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecast = response.data.daily;
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col">
       <div class="next-days">
-        <div class="forecast-day">${day}</div>
+        <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="forecast-temps">
-          <span class="temp-max"> 18° </span>
-          <span class="temp-min"> 12° </span>
+          <span class="temp-max">${Math.round(forecastDay.temp.max)}° </span>
+          <span class="temp-min">${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>  
       </div>
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -146,7 +157,6 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
 
 button.addEventListener("click", handleSubmit);
 locationButton.addEventListener("click", findLocation);
